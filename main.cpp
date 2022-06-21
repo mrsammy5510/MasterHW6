8ff3e1682c3b73349f94aac4115f7f4066129e34
mrsammy5510 <72500339+mrsammy5510@users.noreply.github.com>
2022-06-21 19:21:02 +0800

    captured numbers to variable "node" and "linking_node"

--- main.cpp
+++ main.cpp
@@ -4,15 +4,19 @@ int main()
 {
     const char* filename = "facebook_combined.txt";
     FILE* input_file = fopen(filename, "r");
+    int node, linking_node;
     if (!input_file)
         exit(EXIT_FAILURE);
 
     char *contents = NULL;
     size_t len = 0;
-    while (getline(&contents, &len, input_file) != -1){
-        printf("%s", contents);
+    /*while (getline(&contents, &len, input_file) != -1){
+        printf("%d %d", contents);
+    }*/
+    while(fscanf(input_file, "%d %d", &node, &linking_node)!= -1)
+    {
+        printf("%d %d ",node,linking_node);
     }
-
     fclose(input_file);
     free(contents);
 
