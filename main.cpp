b278799325ccb2ae3b0daacfac2e314385cbd097
mrsammy5510 <72500339+mrsammy5510@users.noreply.github.com>
2022-06-23 19:32:04 +0800

    building graph using struct array
    The data will be presented as -1 at the end of struct linked list。

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
+
+struct column
+{
+    int data;        //沒有data
+    column* next;
+};
+
 int main()
 {
     const char* filename = "facebook_combined.txt";
     FILE* input_file = fopen(filename, "r");
     int node, linking_node;
+    column graph [5000];
+    column* current;
     if (!input_file)
         exit(EXIT_FAILURE);
 
+    for(int i = 0;i<=4999;i++)
+    {
+        graph[i].data = -1;
+    }
     char *contents = NULL;
     size_t len = 0;
     /*while (getline(&contents, &len, input_file) != -1){
@@ -15,7 +28,23 @@ int main()
     }*/
     while(fscanf(input_file, "%d %d", &node, &linking_node)!= -1)
     {
-        printf("%d %d ",node,linking_node);
+        current = &graph[node];
+        printf("%d",current->data);
+        if(graph[node].data == -1) //還未寫入過連接點
+        {
+            graph[node].data = linking_node;
+            graph[node].next = (column*)malloc(sizeof(column));
+            (graph[node].next)->data = -1;
+        }
+        else //已寫入過
+        {
+            while(current->data != -1)
+                current = current->next;
+
+            current->data = linking_node;
+            current->next = (column*)malloc(sizeof(column));
+            (current->next)->data = -1;
+        }
     }
     fclose(input_file);
     free(contents);
