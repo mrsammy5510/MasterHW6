4ea14e52308e35e3a49cf01574d792ea5f72c507
mrsammy5510 <72500339+mrsammy5510@users.noreply.github.com>
2022-06-24 19:35:43 +0800

    BFS search complete

--- main.cpp
+++ main.cpp
@@ -1,35 +1,75 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+#define QUEUE_SIZE 5000
 struct column
 {
-    int data;        //沒有data
+    int data;        
     column* next;
 };
-int queue[5000] = {'\0'};
-int visited[5000] = {0};
+int queue[QUEUE_SIZE] = {'\0'};
+int visited[QUEUE_SIZE] = {0};
 int front = 0;
 int rear = 0;
+column* current;
+column graph [5000];
 
-void BFS(column* graph,int vertex)
+int Isemptyqueue(void)          //無法分辨Q滿或Q空
 {
-    
+    if(rear==front)
+        return 0;               //迴圈的判斷條件是0或非0
+    else
+        return 1;
 }
 void enqueue(int data)
 {
-
+    rear = (rear+1)%QUEUE_SIZE;
+    if(rear==front)
+        return;
+    else
+        queue[rear] = data;
 }
 int dequeue(void)
 {
-    
+    int data;
+    if(rear == front)
+        return -1;
+    else
+    {
+        front = (front+1)%QUEUE_SIZE;
+        data = queue[front];
+        return data;
+    }
+}
+void BFS(int vertex)
+{
+    int bfs_now;
+    visited[vertex] = 1;
+    enqueue(vertex);
+    while(Isemptyqueue())
+    {
+        bfs_now = dequeue();
+        printf("%d ",bfs_now);
+        current = &graph[bfs_now];
+
+        while(current->data!=-1)
+        {
+            if(visited[current->data]!=1)
+            {
+                enqueue(current->data);
+                visited[current->data] = 1;
+            }
+            current = current->next;
+        }
+        
+    }
 }
 int main()
 {
     const char* filename = "facebook_combined.txt";
     FILE* input_file = fopen(filename, "r");
-    int node, linking_node,vertex=0;
-    column graph [5000];
-    column* current;
+    int node, linking_node,vertex;
+    
+    
     if (!input_file)
         exit(EXIT_FAILURE);
 
@@ -42,7 +82,7 @@ int main()
 
     while(fscanf(input_file, "%d %d", &node, &linking_node)!= -1)
     {
-        current = &graph[node];
+        current = &graph[node];                                     //node edge
         if(graph[node].data == -1) //還未寫入過連接點
         {
             graph[node].data = linking_node;
@@ -58,10 +98,28 @@ int main()
             current->next = (column*)malloc(sizeof(column));
             (current->next)->data = -1;
         }
+
+        current = &graph[linking_node];                             //linking node edge
+        if(graph[linking_node].data == -1) //還未寫入過連接點
+        {
+            graph[linking_node].data = node;
+            graph[linking_node].next = (column*)malloc(sizeof(column));
+            (graph[linking_node].next)->data = -1;
+        }
+        else //已寫入過
+        {
+            while(current->data != -1)
+                current = current->next;
+
+            current->data = node;
+            current->next = (column*)malloc(sizeof(column));
+            (current->next)->data = -1;
+        }
     }
     fclose(input_file);
     free(contents);
-    
-    BFS(graph,vertex);
+    printf("輸入要做BFS和DFS的數字:");
+    scanf("%d",&vertex);
+    BFS(vertex);
     exit(EXIT_SUCCESS);
 }
